524ce10da176cfeb2011371aef0415716ae0370b
oAndin <00andherson@gmail.com>
2023-06-27 15:23:34 -0300

    Progress

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const canvasEvent = document.querySelector('canvas'),
-canvasCtx = canvas.getContext("2d");
+// starting the project getting both tags that im going to work with
+const canvasEvent = document.querySelector('canvas'), canvasCtx = canvasEvent.getContext('2d');
+const lineWidth = 15;
+
 
 function setUp(){
-    canvasEvent.width = window.innerWidth;
-    canvasCtx.width = window.innerWidth;
-    canvasEvent.height = window.innerHeight;
-    canvasCtx.height = window.innerHeight;
+// getting the screen width and height for canvas
+    canvasEvent.width = canvasCtx.width = window.innerWidth;
+    canvasEvent.height = canvasCtx.height = window.innerHeight;
 };
-function drawe(){
+function draw(){
+// canvas properties to create the "field"  
+    canvasCtx.fillStyle = '#004a00';
+    canvasCtx.fillRect (0,0, window.innerWidth, window.innerHeight);
+    canvasCtx.fillStyle = '#ffffff';
+    const x =  window.innerWidth / 2 - lineWidth / 2;
+    const y = 0;
+    const w = lineWidth;
+    const h = window.innerHeight;
+    canvasCtx.fillRect(x, y, w, h);
+};
 
-};
+setUp();
+draw();
