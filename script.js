de2f2a738dabba70278207b3ed836e9363ad3fc3
oAndin <00andherson@gmail.com>
2023-06-27 13:45:31 -0300

    Work with canvas

---
+++ script.js
@@ -0,0 +1,12 @@
+const canvasEvent = document.querySelector('canvas'),
+canvasCtx = canvas.getContext("2d");
+
+function setUp(){
+    canvasEvent.width = window.innerWidth;
+    canvasCtx.width = window.innerWidth;
+    canvasEvent.height = window.innerHeight;
+    canvasCtx.height = window.innerHeight;
+};
+function drawe(){
+
+};
