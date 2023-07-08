79fa493ff1b0834ee3853899bbdb4438503614d6
oAndin <00andherson@gmail.com>
2023-07-07 23:05:47 -0300

    Creating objects

--- script.js
+++ script.js
@@ -1,39 +1,95 @@
 // starting the project getting both tags that im going to work with
-const canvasEvent = document.querySelector('canvas'), canvasCtx = canvasEvent.getContext('2d');
-const lineWidth = 15;
+const canvasEvent = document.querySelector('canvas'),
+    canvasCtx = canvasEvent.getContext('2d'),
+    gapX = 10
+    ;
 
-
-function setUp(){
-// getting the screen width and height for canvas
-    canvasEvent.width = canvasCtx.width = window.innerWidth;
-    canvasEvent.height = canvasCtx.height = window.innerHeight;
+const field = {
+    w: window.innerWidth,
+    h: window.innerHeight,
+    draw: function () {
+        canvasCtx.fillStyle = '#004a00';
+        canvasCtx.fillRect(0, 0, this.w, this.h)
+    }
 };
-function draw(){
-// canvas properties to create the "field"  
-    canvasCtx.fillStyle = '#004a00';
-    canvasCtx.fillRect (0,0, window.innerWidth, window.innerHeight)
-// canvas properties to create the line in the mid field
-    canvasCtx.fillStyle = '#ffffff';
-    canvasCtx.fillRect(window.innerWidth / 2 - lineWidth / 2, 0, lineWidth, window.innerHeight);
-//  canvas properties to draw the left bar 
-    canvasCtx.fillStyle = '#ffffff';
-    canvasCtx.fillRect(10, window.innerHeight/2, lineWidth, window.innerHeight/5);
 
-    // canvas properties to draw the right bar 
-    canvasCtx.fillStyle = '#ffffff';
-    canvasCtx.fillRect(window.innerWidth - lineWidth - 15, window.innerHeight / 2 , lineWidth, window.innerHeight/5);
-    // canvas properties to draw the ball
-    canvasCtx.beginPath()
-    canvasCtx.arc(500, 300, 20, 0, 2 * Math.PI, false);
-    canvasCtx.fill();
+const line = {
+    w: 15,
+    h: field.h,
+    draw: function () {
+        canvasCtx.fillStyle = '#ffffff';
+        canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, this.h);
+    }
+};
+
+const leftPaddle = {
+    x: gapX,
+    y: 100,
+    w: line.w,
+    h: 200,
+    draw: function () {
+        canvasCtx.fillRect(this.x, this.y, this.w, this.h)
+    }
+};
+
+const rightPaddle = {
+    x: field.w - line.w - gapX,
+    y: 100,
+    w: line.w,
+    h: 200,
+    draw: function () {
+        canvasCtx.fillRect(this.x, this.y, this.w, this.h)
+    }
+};
+
+const score = {
+    human: 1,
+    computer: 2,
+    draw: function () {
+        canvasCtx.font = "bold 5rem Arial"
+        canvasCtx.textAlign = 'center'
+        canvasCtx.textBaseline = 'top'
+        canvasCtx.fillStyle = '#00000050'
+        canvasCtx.fillText(this.human, field.w / 4, 50);
+        canvasCtx.fillText(this.computer, field.w / 4 + field.w / 2, 50);
+    }
+};
+
+const ball = {
+    x: 300,
+    y: 200,
+    r: 20,
+    _move: function(){
+        this.x += 1;
+        this.y += 1;
+    },
+    draw: function () {
+        canvasCtx.fillStyle = '#ffffff';
+        canvasCtx.beginPath()
+        canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        canvasCtx.fill();
+        this._move();
+    }
+};
+
+
+function setUp() {
+    // getting the screen width and height for canvas
+    canvasEvent.width = canvasCtx.width = field.w;
+    canvasEvent.height = canvasCtx.height = field.h;
+};
+function draw() {
+    field.draw();
+    line.draw();
+    leftPaddle.draw();
+    rightPaddle.draw();
+    score.draw();
+    ball.draw();
     // canvas properties to draw the scoring
-    canvasCtx.font = "bold 5rem Arial"
-    canvasCtx.textAlign = 'center'
-    canvasCtx.textBaseline = 'top'
-    canvasCtx.fillStyle =  '#00000050'
-    canvasCtx.fillText ('1', window.innerWidth / 4, 50); 
-    canvasCtx.fillText ('3', window.innerWidth / 4 + window.innerWidth/2, 50); 
+   
 };
+
+window.setInterval(draw, 1000 / 60)
 
 setUp();
 draw();
